ba46b1c297031953ebac22a90dad937b1b251f20
Lk10 <62957566+GuestAUser@users.noreply.github.com>
2024-03-20 00:34:47 -0300

    Update stringManipTest.java

--- uniCodes/Java/Solved/stringManipTest.java
+++ uniCodes/Java/Solved/stringManipTest.java
@@ -7,22 +7,19 @@ public class stringManipTest {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Type Athletes Age [dd/mm/aaaa]: ");
         String athleteAge = scanner.nextLine();
-
         String[] date = athleteAge.split("/");
         int day = Integer.parseInt(date[0]);
         int month = Integer.parseInt(date[1]);
         int year = Integer.parseInt(date[2]);
-
         Date currentDate = new Date();
         int currentDay = currentDate.getDay();
         int currentMonth = currentDate.getMonth();
         int currentYear = currentDate.getYear() + 1900;
-
         int age = currentYear - year;
+        
         if (currentMonth < month || (currentMonth == month && currentDay < day)) {
             age--;
         }
-
         System.out.println("The athlete is " + age + " years old.");
         scanner.close();
     }
