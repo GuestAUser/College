a34039e02b272ff2199a51db875f54ee3f1647e2
Lk10 <62957566+GuestAUser@users.noreply.github.com>
2024-04-02 11:43:09 -0300

    Create HSVC.java

---
+++ uniCodes/Java/Solved/HSVC.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CalculoRoubo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int[] dinheiroPessoas = new int[5];
+        int totalRoubado = 0;
+        boolean checkVerdadeiro = false;
+        
+        System.out.println("Digite o dinheiro das 5 pessoas presas:");
+        for (int i = 0; i < 5; i++) {
+            dinheiroPessoas[i] = scanner.nextInt();
+        }
+        System.out.println("Digite o dinheiro que o bandido que se entregou roubou:");
+        int dinheiroMenosRoubou = scanner.nextInt();
+        
+        while (checkVerdadeiro == false) {
+            System.out.println("Informacao falsa!");
+            dinheiroMenosRoubou = scanner.nextInt();
+            if (dinheiroMenosRoubou % 10 == 0) {
+                checkVerdadeiro = true;
+            }
+        }
+        
+        for (int dinheiro : dinheiroPessoas) {
+            if (dinheiro % 10 == 0 && dinheiro > dinheiroMenosRoubou) {
+                totalRoubado += dinheiro;
+            }
+        }
+        
+        System.out.println("O total de dinheiro roubado foi: " + totalRoubado);
+    }
+}
