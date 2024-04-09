8038e6c45ee22eac19a71c525952dff36e1efade
Lk10 <62957566+GuestAUser@users.noreply.github.com>
2024-04-09 10:37:21 -0300

    Add files via upload

--- uniCodes/Java/Solved/atividade.java
+++ uniCodes/Java/Solved/atividade.java
@@ -1,9 +1,13 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
+//Comentários estão só pra deixar a leitura mais simples possível em caso de uso para estudos.
+//Code Made by: Lk10 Github(GuestAUser);
+
+
+//Chamar main
 public class Main {
     public static void main(String[] args) {
-        JogadorFutebol jogador1 = new JogadorFutebol("Cristiano Ronaldo", "Atacante", 36, 1.87, 83.5);
+        JogadorFutebol jogador1 = new JogadorFutebol("Cristiano Neylaldo", "Atacante", 45, 2.20, 120);
         jogador1.mostrarDados();
         jogador1.tempoParaAposentar();
     }
@@ -16,14 +20,16 @@ class JogadorFutebol {
     private double altura;
     private double peso;
     private static final Map<String, Integer> IDADE_APOSENTADORIA;
-
+	
+	//Definindo a estrutura do hashmap;
     static {
         IDADE_APOSENTADORIA = new HashMap<>();
-        IDADE_APOSENTADORIA.put("defesa", 40);
-        IDADE_APOSENTADORIA.put("meio-campo", 38);
-        IDADE_APOSENTADORIA.put("atacante", 36);
+        IDADE_APOSENTADORIA.put("defesa", 40); //Objeto->Atributo;
+        IDADE_APOSENTADORIA.put("meio-campo", 38); //Objeto->Atributo;
+        IDADE_APOSENTADORIA.put("atacante", 36); //Objeto->Atributo;
     }
-
+	
+	//Definindo estrutura pública de nomes para definir o objeto(Jogador);
     public JogadorFutebol(String nome, String posicao, int idade, double altura, double peso) {
         this.nome = nome;
         this.posicao = posicao;
@@ -31,15 +37,17 @@ class JogadorFutebol {
         this.altura = altura;
         this.peso = peso;
     }
-
+	
+	//Como os dados vão ser mostrados;
     public void mostrarDados() {
         System.out.println("Nome: " + nome);
         System.out.println("Posição: " + posicao);
-        System.out.println("Idade: " + idade);
-        System.out.println("Altura: " + altura);
-        System.out.println("Peso: " + peso);
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Altura: " + altura + " m");
+        System.out.println("Peso: " + peso + " kg");
     }
-
+	
+	//Definir-> {Limite de tempo referente a [aposentadoria]}.
     public void tempoParaAposentar() {
         int idadeAposentadoria = IDADE_APOSENTADORIA.getOrDefault(posicao.toLowerCase(), -1);
         if (idadeAposentadoria == -1) {
