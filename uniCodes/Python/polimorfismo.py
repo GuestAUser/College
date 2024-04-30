e6d1d95c3c03840e55a98e2202e2dc88f6446081
Lk10 <62957566+GuestAUser@users.noreply.github.com>
2024-04-30 10:11:42 -0300

    Update polimorfismo.py

--- uniCodes/Python/polimorfismo.py
+++ uniCodes/Python/polimorfismo.py
@@ -4,32 +4,32 @@ class Tool:
 
 class Hammer(Tool):
     def action(self):
-        return "*You have used the Hammer!"
+        return " * You have used the Hammer!"
 
 class Screwdriver(Tool):
     def action(self):
-        return "You have used the Screwdriver!"
+        return " * You have used the Screwdriver!"
 
 class Wrench(Tool):
     def action(self):
-        return "You have used the Wrench!"
+        return " * You have used the Wrench!"
 
 class Toolbox:
+    tools = {
+        "hammer": Hammer,
+        "screwdriver": Screwdriver,
+        "wrench": Wrench
+    }
+
     @staticmethod
     def get_tool(tool_type):
-        if tool_type == "hammer":
-            return Hammer()
-        elif tool_type == "screwdriver":
-            return Screwdriver()
-        elif tool_type == "wrench":
-            return Wrench()
-        else:
-            return None
+        return Toolbox.tools.get(tool_type.lower())
 
 tool_type = input("What tool do you want to use? (hammer/screwdriver/wrench): ").lower()
-tool = Toolbox.get_tool(tool_type)
+tool_class = Toolbox.get_tool(tool_type)
 
-if tool:
+if tool_class:
+    tool = tool_class()
     print(tool.action())
 else:
-    print("Desculpe, essa ferramenta não está disponível.")
+    print("Sorry, tool not available.")
